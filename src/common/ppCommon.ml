13ffbccedd25f09618f03367473cbd4252b0795b
Frédéric Dabrowski <33789275+DabrowskiFr@users.noreply.github.com>
2022-06-20 10:52:31 +0200

    add pretty printer for mir

--- src/common/ppCommon.ml
+++ src/common/ppCommon.ml
@@ -5,6 +5,9 @@ open TypesCommon
 
 
 let pp_comma (pf : formatter) (() : unit) : unit = Format.fprintf pf "," 
+
+let pp_semi (pf : formatter) (() : unit) : unit = Format.fprintf pf ";" 
+
   let pp_field (pp_a : formatter -> 'a -> unit) (pf : formatter) ((x,y) : string * 'a) = 
     Format.fprintf pf "%s:%a" x pp_a y
 
