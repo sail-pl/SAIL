4cfadf6a7362adcba6cca8d109908f889293005d
Térence Clastres <terence.clastres@etu.univ-orleans.fr>
2022-07-17 00:33:01 +0200

    error : gracefully handle dummy_pos

--- src/common/error.ml
+++ src/common/error.ml
@@ -1,10 +1,10 @@
 open Monad
 
+open Lexing
 module E = MenhirLib.ErrorReports
 
 
 let show_context text (p1,p2) =
-  let open Lexing in
   let p1 = { p1 with pos_cnum=p1.pos_bol}
   and p2 = { p2 with pos_cnum= String.index_from text p2.pos_cnum '\n'}  in
   E.extract text (p1,p2) |> E.sanitize
@@ -18,11 +18,17 @@ type 'a result = ('a, error_type) Result.t
 let print_errors (file:string) (errs:error_type) : unit =  
   let s = List.fold_left (
     fun s (l,msg) ->
-      let location = MenhirLib.LexerUtil.range l in
-      let indication = show_context file l in 
-      let start = String.make ((fst l).pos_cnum - (fst l).pos_bol )' ' in
-      let ending = String.make ((snd l).pos_cnum - (fst l).pos_cnum )'~' in
-      Printf.sprintf "\n%s\n\t%s\n\t%s%s\n%s\n\n%s" location indication start ending msg s
+      if l = (dummy_pos,dummy_pos) then
+        (
+        Logs.warn (fun m -> m "No location for error");
+        msg
+        )
+      else
+        let location = MenhirLib.LexerUtil.range l in
+        let indication = show_context file l in 
+        let start = String.make ((fst l).pos_cnum - (fst l).pos_bol )' ' in
+        let ending = String.make ((snd l).pos_cnum - (fst l).pos_cnum )'~' in
+        Printf.sprintf "\n%s\n\t%s\n\t%s%s\n%s\n\n%s" location indication start ending msg s
   )
   String.empty errs in
   Logs.err (fun m -> m "found %i error(s) : \n%s" (List.length errs) s)
