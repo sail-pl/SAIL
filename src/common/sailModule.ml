fbce876c860b937eb0ae3c44f93d0fd4fcd9af98
Térence Clastres <terence.clastres@etu.univ-orleans.fr>
2023-05-31 00:35:28 +0200

    allow cross-compilation, simple  init-loop mode

--- src/common/sailModule.ml
+++ src/common/sailModule.ml
@@ -45,3 +45,23 @@ let emptyModule =
     libs = FieldSet.empty
   };
 }
+
+let method_decl_of_defn (d : 'a method_defn) : Declarations.method_decl = 
+  let pos = d.m_proto.pos
+  and name = (match d.m_body with Left (sname,_) -> sname | Right _ -> d.m_proto.name)
+  and ret = d.m_proto.rtype 
+  and args = d.m_proto.params 
+  and generics = d.m_proto.generics 
+  and variadic = d.m_proto.variadic in
+  ((pos,name),{ret;args;generics;variadic})
+  
+
+open Monad.MonadSyntax(Error.Logger)
+let method_of_process (m : 'a t) (name:string) : 'a method_defn Error.Logger.t = 
+  let+ p = Error.Logger.throw_if_none 
+      (Error.make dummy_pos @@ "module '" ^ m.md.name ^ "' : no '" ^ name ^ "' process found")
+      (List.find_opt (fun p -> p.p_name = name) m.processes) 
+  in
+  let m_proto = {pos=p.p_pos; name=String.lowercase_ascii p.p_name; generics = p.p_generics; params = fst p.p_interface; variadic=false; rtype=None} 
+  and m_body = Either.right p.p_body in
+  {m_proto;m_body}
